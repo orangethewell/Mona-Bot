82e8934ec50bef7233a1cff27cd186669e78a3a6
orangethewell <sempjunior05@gmail.com>
2021-08-08 00:01:00 -0300

    Reimplement loop

--- main.py
+++ main.py
@@ -662,4 +662,6 @@ async def background_up_task():
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     loop.create_task(background_up_task())
-    loop.run_until_complete(setup_bot())
+    loop.create_task(setup_bot())
+
+    loop.run_forever()
